53221687c5fde2d7b3cdc72a38e08712590070b3
AgusRodriguez152 <111789054+AgusRodriguez152@users.noreply.github.com>
2022-09-02 15:15:41 -0300

    delete

--- ProyectoFinal/LigaFc/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import * 
-
-# Register your models here.
-admin.site.register(Goleadores)
-admin.site.register(Asistidores)
-admin.site.register(Amarillas)
-admin.site.register(Rojas)
